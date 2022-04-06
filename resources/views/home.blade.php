9bf48574f9845a698dfdd1ab9b632eaa93673693
Roza Nawzad <rozanawzad2001@gmail.com>
2022-04-06 11:16:27 -0700

    rrr

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,7 @@
 <html>
 
-
+<h1> Hello There This Is Your Home Page </h1><br>
+to see my profile click here!
 <a href="http://127.0.0.1:8000/profile">profile</a>
 
 
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -2,18 +2,10 @@
 <html>
 <body>
 
-<h1>The textarea element</h1>
-
-<form action="/action_page.php">
-<label for="w3review">Review of W3Schools:</label>
-<textarea id="w3review" name="w3review" rows="4" cols="50">
-  At w3schools.com you will learn how to make a website. They offer free tutorials in all web development technologies.
-  </textarea>
-  <br><br>
-  <input type="submit" value="Submit">
-</form>
-
-<p>Click the "Submit" button and the form-data will be sent to a page on the 
-server called "action_page.php".</p>
+<h1>profile page</h1>
+<label><img src="{{ URL('image/download.jpg') }}"  width="100" height="100"> </label><br> <br>
+<label> Name: Roza Nawzad Salih</label><br>
+<label> Birthday: 16-Mar-2001</label><br>
+<label> Department: IT</label>
 </body>
 </html>
